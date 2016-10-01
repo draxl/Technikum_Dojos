2c197e345c664743b64112fec464b92799b4f72c
draxl <Dresden2013*>
2016-10-01 13:56:57 +0200

    Initial commit

---
+++ Draxl_Dojo2/Draxl_Dojo2/Classes/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draxl_Dojo2.Classes
+{
+    class Stack
+    {
+        public Stack()
+        {
+
+        }
+
+        public 
+
+    }
+}
